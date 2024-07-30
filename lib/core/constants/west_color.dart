010a0975b7179708236db4c9070b1417946304b7
유지우 <treeyu12345@naver.com>
2024-07-30 19:00:41 +0900

    ADD :: 폰트 추가

---
+++ lib/core/constants/west_color.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class WESTColor {
+  WESTColor._();
+
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color error = const Color(0xFFFF6363);
+  static Color background = const Color(0xFF111111);
+
+  static Color main50 = const Color(0xFFF5FFE0);
+  static Color main100 = const Color(0xFFE6FFB3);
+  static Color main200 = const Color(0xFFD6FF80);
+  static Color main300 = const Color(0xFFC6FF4D);
+  static Color main400 = const Color(0xFFB9FF26);
+  static Color main500 = const Color(0xFFADFF00);
+  static Color main600 = const Color(0xFFA6FF00);
+  static Color main700 = const Color(0xFF9CFF00);
+  static Color main800 = const Color(0xFF93FF00);
+
+  static Color gray50 = const Color(0xFFE5E5E5);
+  static Color gray100 = const Color(0xFFBDBDBD);
+  static Color gray200 = const Color(0xFF929292);
+  static Color gray300 = const Color(0xFF666666);
+  static Color gray400 = const Color(0xFF454545);
+  static Color gray500 = const Color(0xFF242424);
+  static Color gray600 = const Color(0xFF202020);
+  static Color gray700 = const Color(0xFF1B1B1B);
+  static Color gray800 = const Color(0xFF161616);
+}

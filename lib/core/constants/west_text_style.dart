8f2f08def6eab46f0c9f539fc962c5eb1e757626
유지우 <treeyu12345@naver.com>
2024-07-30 19:01:18 +0900

    SETTING :: 폰트 스타일 컴포넌트 추가

---
+++ lib/core/constants/west_text_style.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+class WESTTextStyle {
+  WESTTextStyle._();
+
+  static TextStyle heading1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 42,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow
+    );
+  }
+
+  static TextStyle heading2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 32,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow
+    );
+  }
+
+  static TextStyle heading3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle heading4({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle caption1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle caption2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle button1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle button2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle label1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle label2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      overflow: overflow,
+    );
+  }
+}

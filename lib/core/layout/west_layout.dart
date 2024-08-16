73dec5a24cd320c1d099640bd15623eb2bb4226f
유지우 <treeyu12345@naver.com>
2024-08-16 16:20:33 +0900

    FEAT :: 기본 레이아웃 추가

---
+++ lib/core/layout/west_layout.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:west_flutter_v1/core/constants/west_style.dart';
+
+class WESTLayout extends StatelessWidget {
+  final Color? backgroundColor;
+  final PreferredSizeWidget? appBar;
+  final Widget child;
+  final Widget? floatingActionButton, bottomSheet;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+
+  const WESTLayout({
+    super.key,
+    required this.child,
+    this.backgroundColor,
+    this.appBar,
+    this.floatingActionButton,
+    this.floatingActionButtonLocation,
+    this.bottomSheet,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor ?? WESTColor.background,
+      appBar: appBar,
+      body: SafeArea(
+        child: child,
+      ),
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation: floatingActionButtonLocation,
+      bottomSheet: bottomSheet,
+    );
+  }
+}

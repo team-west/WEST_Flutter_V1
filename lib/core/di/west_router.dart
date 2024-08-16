4e5c734123e1a3b53ebc82087ab099aae090ac6f
유지우 <treeyu12345@naver.com>
2024-08-16 16:20:15 +0900

    ADD :: 라우터 설정

--- lib/core/di/west_router.dart
+++ lib/core/di/west_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:west_flutter_v1/presentation/sign_in/view/sign_in_screen.dart';
-import 'package:west_flutter_v1/presentation/sign_up/view/sign_up_screen.dart';
+import 'package:west_flutter_v1/presentation/sign_up/view/sign_up_gender_screen.dart';
+import 'package:west_flutter_v1/presentation/sign_up/view/sign_up_user_info_screen.dart';
 
 final GoRouter westRouter = GoRouter(
   initialLocation: "/",
@@ -10,8 +11,12 @@ final GoRouter westRouter = GoRouter(
       builder: (context, state) => const SignInScreen(),
     ),
     GoRoute(
-      path: "/signUp",
-      builder: (context, state) => const SignUpScreen(),
+      path: "/signUpUserInfo",
+      builder: (context, state) => const SignUpUserInfoScreen(),
+    ),
+    GoRoute(
+      path: "/signUpGender",
+      builder: (context, state) => const SignUpGenderScreen(),
     ),
   ],
 );

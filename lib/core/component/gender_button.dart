0c67e96f44b6d4d600859796c744bed8b15d3a72
유지우 <treeyu12345@naver.com>
2024-09-27 19:29:14 +0900

    FEAT :: 성별 선택 버튼 추가

---
+++ lib/core/component/gender_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:west_flutter_v1/core/constants/west_style.dart';
+import 'package:west_flutter_v1/core/provider/gender_select_provider.dart';
+import 'package:west_flutter_v1/core/provider/state/gender_type.dart';
+
+class GenderButton extends ConsumerWidget {
+  final String type;
+  final GenderType gender;
+
+  const GenderButton({
+    super.key,
+    required this.type,
+    required this.gender,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedGender = ref.watch(genderSelectProvider);
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => ref.read(genderSelectProvider.notifier).state = gender,
+        child: Container(
+          height: 60,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: gender == selectedGender? WESTColor.main300 : WESTColor.gray300,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Text(
+            type,
+            style: WESTTextStyle.button1(
+              color: gender == selectedGender ? WESTColor.black : WESTColor.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

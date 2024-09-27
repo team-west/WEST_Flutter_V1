bc9074f4e8893b94805e28cb5d7d4c204d5e8779
유지우 <treeyu12345@naver.com>
2024-09-27 20:12:40 +0900

    ANNO :: 주석 추가

--- lib/presentation/sign_up/widget/sign_up_app_bar.dart
+++ lib/presentation/sign_up/widget/sign_up_app_bar.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:west_flutter_v1/core/constants/west_style.dart';
 
 class SignUpAppBar extends StatelessWidget implements PreferredSizeWidget {
+  /// 페이지 번호
   final int count;
+
+  /// 뒤로가기 라우터를 적용할 페이지
+  final String popRoute;
 
   const SignUpAppBar({
     super.key,
     required this.count,
+    required this.popRoute,
   });
 
   @override
@@ -15,28 +22,48 @@ class SignUpAppBar extends StatelessWidget implements PreferredSizeWidget {
       child: PreferredSize(
         preferredSize: preferredSize,
         child: AppBar(
+          /// 자동으로 Leading Widget이 생성되지 않도록 설정
           automaticallyImplyLeading: false,
+
+          /// 스크롤 할 시 아래의 Elevation 설정
           scrolledUnderElevation: 0,
           backgroundColor: WESTColor.background,
-          title: Align(
-            alignment: Alignment.centerRight,
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "$count",
-                    style: WESTTextStyle.subTitle2(
-                      color: WESTColor.main500,
+
+          /// title 위젯의 기본 간격 해제
+          titleSpacing: 0,
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    context.pop(popRoute);
+                  },
+                  child: SvgPicture.asset("$iconCoreAsset/left_arrow_icon.svg"),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$count",
+                          style: WESTTextStyle.subTitle2(
+                            color: WESTColor.main500,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "/3",
+                          style: WESTTextStyle.subTitle2(
+                            color: WESTColor.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  TextSpan(
-                    text: "/3",
-                    style: WESTTextStyle.subTitle2(
-                      color: WESTColor.white,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),

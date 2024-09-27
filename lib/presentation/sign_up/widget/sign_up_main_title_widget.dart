13a927ac769963e52f3af2a014167b816ee82dca
유지우 <treeyu12345@naver.com>
2024-09-27 20:13:00 +0900

    FEAT :: 회원가입 페이지에서 공통으로 쓰이는 제목 위젯 추가

---
+++ lib/presentation/sign_up/widget/sign_up_main_title_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:west_flutter_v1/core/constants/west_style.dart';
+
+class SignUpMainTitleWidget extends StatelessWidget {
+  const SignUpMainTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "회원가입하고\nWEST를 사용해보세요!",
+      style: WESTTextStyle.heading3(
+        color: WESTColor.white,
+      ),
+    );
+  }
+}

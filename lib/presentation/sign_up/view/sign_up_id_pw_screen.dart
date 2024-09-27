0a8b1baf3242073df7e7e6347481adbf83941a3b
유지우 <treeyu12345@naver.com>
2024-09-27 20:12:22 +0900

    FEAT :: 회원가입 아이디 비밀번호 입력 페이지 추가

---
+++ lib/presentation/sign_up/view/sign_up_id_pw_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:west_flutter_v1/core/component/west_button.dart';
+import 'package:west_flutter_v1/core/component/west_text_field.dart';
+import 'package:west_flutter_v1/core/constants/west_style.dart';
+import 'package:west_flutter_v1/core/layout/west_layout.dart';
+import 'package:west_flutter_v1/presentation/sign_in/provider/sign_in_text_field_focus_provider.dart';
+import 'package:west_flutter_v1/presentation/sign_up/widget/sign_up_app_bar.dart';
+import 'package:west_flutter_v1/presentation/sign_up/widget/sign_up_main_title_widget.dart';
+import 'package:west_flutter_v1/presentation/sign_up/widget/sign_up_routes_widget.dart';
+
+class SignUpIdPwScreen extends ConsumerStatefulWidget {
+  const SignUpIdPwScreen({super.key});
+
+  @override
+  ConsumerState<SignUpIdPwScreen> createState() => _SignUpIdPwScreenState();
+}
+
+class _SignUpIdPwScreenState extends ConsumerState<SignUpIdPwScreen> {
+  late TextEditingController idController;
+  late TextEditingController pwdController;
+
+  late FocusNode idFocusNode;
+  late FocusNode pwdFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    idController = TextEditingController();
+    pwdController = TextEditingController();
+
+    idFocusNode = FocusNode()..addListener(chkNode);
+    pwdFocusNode = FocusNode()..addListener(chkNode);
+  }
+
+  @override
+  void dispose() {
+    idController.dispose();
+    pwdController.dispose();
+
+    idFocusNode.dispose();
+    pwdFocusNode.dispose();
+    super.dispose();
+  }
+
+  void chkNode() {
+    ref.read(signInTextFieldFocusProvider.notifier).state =
+        idFocusNode.hasFocus || pwdFocusNode.hasFocus ? true : false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WESTLayout(
+      appBar: const SignUpAppBar(count: 3, popRoute: "/signUpGender"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SignUpRoutesWidget(),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  context.push("/signUpIdPw");
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: WESTButton(
+                    backgroundColor: WESTColor.gray300,
+                    content: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "회원가입",
+                          style: WESTTextStyle.button1(
+                            color: WESTColor.white,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        SvgPicture.asset(
+                          "$iconCoreAsset/long_ended_arrow_icon.svg",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 32),
+                  const SignUpMainTitleWidget(),
+                  const SizedBox(height: 40),
+                  WESTTextField(
+                    title: "아이디",
+                    controller: idController,
+                    focusNode: idFocusNode,
+                  ),
+                  const SizedBox(height: 20),
+                  WESTTextField(
+                    title: "비밀번호",
+                    controller: pwdController,
+                    focusNode: pwdFocusNode,
+                    textInputAction: TextInputAction.done,
+                    password: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

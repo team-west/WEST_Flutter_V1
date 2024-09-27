9896f6a49d9c503336f8f7838ec47a640cd02ddb
유지우 <treeyu12345@naver.com>
2024-09-27 19:31:37 +0900

    FEAT :: 스플래시 화면 추가

---
+++ lib/presentation/splash/view/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:west_flutter_v1/core/constants/west_style.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(milliseconds: 1100)).then((value) => context.go("/"));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: WESTColor.background,
+      body: Center(
+        child: Image.asset(
+          "$imageCoreAsset/west_splash_image.png",
+          width: 240,
+          height: 240,
+        ),
+      ),
+    );
+  }
+}

db29515f436dc721bb87ad46b9c7a9820249449e
유지우 <treeyu12345@naver.com>
2024-09-27 19:30:34 +0900

    ADD :: 페이지 라우트 추가

--- lib/core/di/west_router.dart
+++ lib/core/di/west_router.dart
@@ -2,13 +2,18 @@ import 'package:go_router/go_router.dart';
 import 'package:west_flutter_v1/presentation/sign_in/view/sign_in_screen.dart';
 import 'package:west_flutter_v1/presentation/sign_up/view/sign_up_gender_screen.dart';
 import 'package:west_flutter_v1/presentation/sign_up/view/sign_up_user_info_screen.dart';
+import 'package:west_flutter_v1/presentation/splash/view/splash_screen.dart';
 
 final GoRouter westRouter = GoRouter(
-  initialLocation: "/",
+  initialLocation: "/splash",
   routes: [
     GoRoute(
       path: "/",
       builder: (context, state) => const SignInScreen(),
+    ),
+    GoRoute(
+      path: "/splash",
+      builder: (context, state) => const SplashScreen(),
     ),
     GoRoute(
       path: "/signUpUserInfo",
